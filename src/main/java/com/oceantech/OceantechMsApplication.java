8f3da6456f040132d4e0f5a0521a56d9de199be1
Danilo Manchon <danilomanchon@gmail.com>
2021-11-04 18:44:35 -0300

    first commit

---
+++ src/main/java/com/oceantech/OceantechMsApplication.java
@@ -0,0 +1,13 @@
+package com.oceantech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceantechMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OceantechMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oceantech/OceantechMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceantech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceantechMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
